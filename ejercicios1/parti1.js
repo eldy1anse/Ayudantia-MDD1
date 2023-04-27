cc59c80fbbfff94d390e35f290357b5046ab5c19
Dylan Suazo E <dylan.suazo2101@alumnos.ubiobio.cl>
2023-04-26 23:06:35 -0400

    Termine el ejercicio 1

--- ejercicios1/parti1.js
+++ ejercicios1/parti1.js
@@ -1,2 +1,11 @@
 //Declara una variable o constante llamada nombre y asígnale un valor de tipo cadena.
 const nombre = 'Jordi';
+var edad = 20;
+var puedoConducir = false;
+
+console.log(typeof nombre);
+console.log(nombre)
+console.log(typeof edad);
+console.log(edad);
+console.log(typeof puedoConducir);
+console.log(puedoConducir);
