271f6482a59f9cf497592efaf4d53b4d74d166d7
Dylan Suazo E <dylan.suazo2101@alumnos.ubiobio.cl>
2023-04-26 23:23:15 -0400

    ejercicio 2 (falta correjir para imprimir los resultados de incremento y decremento)

---
+++ ejercicios1/parte2.js
@@ -0,0 +1,21 @@
+//Dado los siguientes valores numéricos:
+const numero1 = 10
+const numero2 = 20
+//imprima el resultado de las siguientes operaciones:
+//Suma
+console.log("Resultado suma = " , numero1 + numero2);
+//Resta
+console.log("Resultado resta = " , numero1 - numero2);
+//Multiplicación
+console.log("Resultado multiplicación = " , numero1 * numero2);
+//División
+console.log("Resultado división = " , numero1 / numero2);
+//Módulo
+console.log("Resultado módulo = " , numero1 % numero2);
+
+const numero3 = numero1++;
+const numero4 = numero1--;
+//Incremento
+console.log("Resultado incremento = " , numero3++);
+//Decremento
+console.log("Resultado decremento = " , numero4--);
